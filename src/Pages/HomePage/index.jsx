7ef840a91aa4f07984a27f8bf8d7bcb25f09d80a
rhaneyko <rhaneykohonorio@gmail.com>
2022-06-17 17:01:21 -0400

    React App

---
+++ src/Pages/HomePage/index.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef, useState } from 'react'
+import CardMovie from '../../components/CardMovie';
+import CharactersMovie from '../../components/CharactersMovie';
+
+import Carousel from 'react-elastic-carousel'
+import { 
+   Container,
+   Title,
+   HeaderMovies,
+   CarouselSlide,
+   Line,
+   HeaderCharacters,
+     } from './styles'
+
+import movies from '../../assets/data/movies.json';
+
+
+
+const HomePage = () => {
+
+
+  return (
+    <Container>
+        <HeaderMovies>
+          <Line/>
+           <Title>Movies</Title>
+          <Line/>
+        </HeaderMovies>
+        <CarouselSlide>
+            <CardMovie />
+            <CardMovie />
+            <CardMovie />
+            <CardMovie />
+        </CarouselSlide>
+         
+        <HeaderCharacters>
+          <Line/>
+           <Title>Characters</Title>
+          <Line/>
+        </HeaderCharacters>
+        <CarouselSlide
+          itemsToShow={3}
+          itemsToScroll={3}
+          
+        >
+          <CharactersMovie/>
+          <CharactersMovie/>
+          <CharactersMovie/>
+          <CharactersMovie/>
+          <CharactersMovie/>
+          <CharactersMovie/>
+          <CharactersMovie/>
+        </CarouselSlide>
+        
+    </Container> 
+  )
+}
+export default HomePage;
